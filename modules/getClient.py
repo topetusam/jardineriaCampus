d94e6ad5087fe6d6fdf958a09b785d26446954c2
topetusam <67940360+topetusam@users.noreply.github.com>
2024-03-01 09:03:24 -0500

    Jardineria

---
+++ modules/getClient.py
@@ -0,0 +1,9 @@
+import storage.cliente as Misclientes
+print(dir())
+
+print(Misclientes)
+def searchName(palabra):
+    print (palabra)
+
+searchName("busque esto plis")
+
---
+++ storage/detalle_pedido.py
@@ -0,0 +1,8 @@
+pedido= [{
+"codigo_pedido": 1,
+"codigo_producto": "",
+"cantidad": 0,
+"precio_unidad": 1.0,
+"numero_linea": 0
+}
+]
