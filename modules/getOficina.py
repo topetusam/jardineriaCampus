5b298357db3508a2705c55151601ebfe9d9edfd9
topetusam <67940360+topetusam@users.noreply.github.com>
2024-03-06 06:55:53 -0500

    consultas de oficina

---
+++ modules/getOficina.py
@@ -0,0 +1,55 @@
+import storage.oficina as of
+
+def getAllCodigoCiudad():
+    codigoCiudad = []
+    for val iimport storage.oficina as of
+
+def getAllCodigoCiudad():
+    codigoCiudad = []
+    for val in of.oficina:
+        codigoCiudad.append({
+            "codigo": val.get("codigo_oficina"),
+            "ciudad": val.get("ciuad")
+        })
+    
+    return codigoCiudad
+
+#devuelve un listado con la ciudad y el telefono 
+#de las oficinas de españa
+
+def getAllCiudadTelefono(pais):
+    ciudadTelefono = []
+    for val in of.oficina:
+        if(val.get("pais") == pais):
+            ciudadTelefono.append({
+                "ciudad": val.get("ciudad")
+                "telefono": val.get("telefono")
+                "oficinas": val.get("codigo_oficina")
+                "pais": val.get("pais")
+
+            })
+        return getAllCiudadTelefono    
+import storage.oficina as of
+
+def getAllCodigoCiudad():
+    codigoCiudad = []
+    for val in of.oficina:
+        codigoCiudad.append({
+            "codigo": val.get("codigo_oficina"),
+            "ciudad": val.get("ciuad")
+        })
+    
+    return codigoCiudad
+
+#devuelve un listado con la ciudad y el telefono 
+#de las oficinas de españa
+
+def getAllCiudadTelefono(pais):
+    ciudadTelefono = []
+    for val in of.oficina:
+        if(val.get("pais") == pais):
+            ciudadTelefono.append({
+                "ciudad": val.get("ciudad")
+                "telefono": val.get("telefono")
+                "oficinas": val.get("codigo_oficina")
+     
