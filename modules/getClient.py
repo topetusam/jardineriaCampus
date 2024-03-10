56cbdcfc2f3c4382ba10ef59f98227fefc222dcf
topetusam <67940360+topetusam@users.noreply.github.com>
2024-03-10 11:05:12 -0500

    añadi los filtros de menu

--- modules/getClient.py
+++ modules/getClient.py
@@ -1,7 +1,9 @@
 import storage.cliente as cli
+from tabulate import tabulate
 
+#devuelve un listado con los nombres de los clientes y el codigo 
 def searchAllClientesName():
-    clienteName = list()
+    clienteName = []
     for val in cli.clientes:
         codigoName=dict({
             "codigo_cliente":val.get('codigo_cliente'),
@@ -9,16 +11,21 @@ def searchAllClientesName():
             }
             )
         clienteName.append(codigoName)
-        return clienteName
+    return clienteName
+#devuelve un listado segun el codigo que le indiquemos al cliente
     
 def getoneClientecodigo(codigo):
-        for val in cli.clientes:
-            if(val.get('codigo_cliente')==codigo):
-                return {
-            "codigo_cliente": val.get('codigo_cliente'),
-            "nombre_cliente": val.get('nombre_cliente')
+    CodigoClient= []
+    for val in cli.clientes:
+        if(val.get('codigo_cliente')==codigo):
+                CodigoClient.append({
+                      "codigo_cliente": val.get('codigo_cliente'),
+                        "nombre_cliente": val.get('nombre_cliente')
+                    
+                })
+    return CodigoClient     
 
-        }
+        
 
 def getAllClientCreditCiudad(limiteCredit, ciudad):
 
@@ -26,10 +33,39 @@ def getAllClientCreditCiudad(limiteCredit, ciudad):
     for val in cli.clientes:
         if(val.get('limite_credito')>= limiteCredit and val.get('ciudad')== ciudad):
                 clienteCredic.append(val)
-        return clienteCredic
+    return clienteCredic
 
+#Obtener informacion de los clientes por pais, region y ciudad
+
+def getAllPaisRegionCiudad(pais, region, ciudad):
+    ClientPaisRegionCiudad= []
+    for val in cli.clientes:
+        if val.get("pais")==pais or val.get("ciudad")==ciudad or val.get("region")==region:
+            ClientPaisRegionCiudad.append({
+                
+                 "pais":val.get("pais"),
+                 "ciudad": val.get("ciudad"),
+                 "region": val.get("region")
+                
+            })
+    
+    return ClientPaisRegionCiudad
 #devuelve un listado con el nombre de todos los clientes españoles
 
+def getAllClientePais(pais):
+     
+     clientePais = []
+     for val in cli.clientes:
+               if val.get("pais")==pais:
+                   clientePais.append({
+                    "nombre": val.get("nombre_cliente"),
+                    "pais":val.get("pais"),
+                    "ciudad": val.get("ciudad"),
+                    "region": val.get("region")
+               })
+     return clientePais
+
+#devuelve clientes Españoles
 
 def getAllNombreClientesEspañoles():
      
@@ -53,9 +89,33 @@ def menu():
           3. Obtener toda la informacion del cliente segun su limite de credito y ciudad que pertenece
           4. Obtener clientes por pais, region y ciudad
           5. Obtener cliente por pais
+          6. Lista los nombres de los clientes españoles
           
           """)
     opcion=int(input("Elija una de las opciones: "))
+    
+    
     if(opcion==1):
         print(tabulate(searchAllClientesName(), headers="keys", tablefmt = 'rounded_grid'))
-         
+    
+    elif(opcion==2):
+        codigoCliente= int(input("Ingrese el codigo del cliente: "))
+        print(tabulate(getoneClientecodigo(codigoCliente), headers="keys", tablefmt = 'rounded_grid'))    
+    
+    elif(opcion == 3):
+        limiteCredit = int(input("Ingrese el limite de credito de los clientes que deseas visualizar: "))
+        ciudad = input("ingrese la ciudad que desea filtrar los clientes: ")
+        print(tabulate(getAllClientCreditCiudad(limiteCredit, ciudad), headers="keys", tablefmt='rounded_grid'))
+     
+    elif(opcion ==4):
+        pais =  input("Ingrese el pais que desea flitrar: ")
+        ciudad = input("ingrese la ciudad que desea filtrar los clientes: ")
+        region = input("Ingrese la region que desea filtrar")
+        print(tabulate(getAllPaisRegionCiudad(pais, region, ciudad), headers="keys", tablefmt='rounded_grid'))
+
+    elif(opcion==5):
+        pais = input("Ingrese el pais que desea flitrar: ")
+        print(tabulate(getAllClientePais(pais), headers="keys", tablefmt='rounded_grid'))
+        
+    elif(opcion==6):
+        print(tabulate(getAllNombreClientesEspañoles(), headers="keys", tablefmt='rounded_grid'))
