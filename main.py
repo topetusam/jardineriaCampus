72605d92dacc0c22e0b30e19b6e6be9a82b3fd80
topetusam <67940360+topetusam@users.noreply.github.com>
2024-03-10 09:35:26 -0500

    se creo el menu para probar

--- main.py
+++ main.py
@@ -13,4 +13,26 @@ import modules.getPedidos as pedido
 print(tabulate(pedido.getAllCodigoEsperadaEntregaPedido(), tablefmt='grid'))
 
 
-
+if (__name__=='__main__'):
+    print("""
+          
+          
+          1. Cliente
+          2. Oficina
+          3. Empleados
+          4. Pedidos
+          5.Pagos 
+          
+          """)
+    opcion = int(input("\n Seleccione unas de las opciones: "))
+    if(opcion==1):
+        cliente.menu()
+    elif(opcion==2):
+        oficina.menu()
+    elif(opcion==3):
+        empleado.menu()
+    elif(opcion==4):
+        pago.menu()
+    elif(opcion==5):
+        pedido.menu()            
+        
--- modules/getClient.py
+++ modules/getClient.py
@@ -43,4 +43,19 @@ def getAllNombreClientesEspañoles():
                     "region": val.get("region")
                })
      return nombreClientesEspañoles
+
+
+def menu():
+    print("""
+          
+          1. Obtener todos los clientes (nombre)
+          2. Obtener un cliente por el codigo
+          3. Obtener toda la informacion del cliente segun su limite de credito y ciudad que pertenece
+          4. Obtener clientes por pais, region y ciudad
+          5. Obtener cliente por pais
+          
+          """)
+    opcion=int(input("Elija una de las opciones: "))
+    if(opcion==1):
+        print(tabulate(searchAllClientesName(), headers="keys", tablefmt = 'rounded_grid'))
          
